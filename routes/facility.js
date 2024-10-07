ffb8840221c4a39ed645d314eb86a44259bc575d
chetanyk655 <printopia06@gmail.com>
2024-10-07 23:22:14 +0530

    New Changes

--- routes/facility.js
+++ routes/facility.js
@@ -27,6 +27,31 @@ router.post("/",(req,res)=>{
     }
 });
 
+router.put('/',(req,res)=>{
+    const email = req.query.email;
+    const id = req.query.id;
+    const {AdminStatus} = req.body;
+
+     const sql = `UPDATE facility SET f_status = "${AdminStatus}" WHERE usr_mail = "${email}" AND f_id="${id}"`
+
+    membersDb.query(sql,(err,result1)=>{
+        if(err){
+            console.log(err);
+            res.status(500).send({
+                "status_code" : 500,
+                "response" : "Internal Server Error"
+            })
+            return;
+        }
+
+        res.status(200).send({
+            "status_code" : 200,
+            "response" : "Successfully updated facility status"
+        })
+       
+    })
+})
+
 router.get("/singleFacility",(req,res)=>{
     const sql = `SELECT * FROM facility WHERE usr_mail = "${req.query.email}"`;
     membersDb.query(sql,(err,result)=>{
@@ -54,6 +79,13 @@ router.get("/",(req,res)=>{
                 "status_code" : 500,
                 "reponse" : "Internal server error"
             });
+        }
+        if(result.length == 0){
+            res.status(404).send({
+                "status_code" : 404,
+                "response" : "No facility enquiries found"
+            })
+            return;
         }
         res.status(200).send({
             "status_code" : 200,
